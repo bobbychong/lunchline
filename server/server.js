5ef5c37835b3fcf1c083de6688b78b18eac762f5
Ethan Rubio <ethanrubio@gmail.com>
2016-03-01 19:43:59 -0800

    converts two views into ionic and adds server dependency for CORS

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var restController = require('./restaurant/restController.js');
+var methodOverride  = require('method-override');
 var jsonParser = require('body-parser').json();
 
 // Require a config file with API keys if testing locally
@@ -15,6 +16,18 @@ var mongoosePassword = process.env.PASSWORD || config.password;
 // Connect to mongo lab account
 mongoose.connect('mongodb://'+mongooseUsername+':'+mongoosePassword+'@ds011168.mlab.com:11168/lunchline')
 console.log('L19 Connected to Mongoose');
+
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+};
+
+app.use(allowCrossDomain);
+app.use(methodOverride());
 
 // Serve static files
 app.use(express.static(__dirname + '/../client/'));//serving all static files to our client folder
