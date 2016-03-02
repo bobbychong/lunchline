5b105c24be77ce108120f8fce8cd8e22582c7c82
Ethan Rubio <ethanrubio@gmail.com>
2016-03-02 15:23:21 -0800

    adds search bar functionality with location and food type

--- lunchline/www/js/services/services.js
+++ lunchline/www/js/services/services.js
@@ -112,6 +112,87 @@ angular.module('lunchline.services', [])
   };
 })
 
-.service('BlankService', [function(){
+.factory('Geolocation', function() {
 
-}])
+  // get userLocation for restaurantView address
+  var userLocation = {};
+
+  var locationInfo = function(callback) {
+    var geocoder;
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+    }
+    //Get the latitude and the longitude;
+    function successFunction(position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        codeLatLng(lat, lng)
+    }
+
+    function errorFunction(){
+        alert('Geocoder failed');
+    }
+
+    function initialize() {
+      geocoder = new google.maps.Geocoder();
+    }
+
+    function codeLatLng(lat, lng) {
+
+      userLocation.lat = lat;
+      userLocation.long = lng;
+
+      var latlng = new google.maps.LatLng(lat, lng);
+      geocoder.geocode({'latLng': latlng}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+        console.log(results)
+          if (results[1]) {
+           //formatted address
+           /*alert(results[0].formatted_address)*/
+          //find country name
+          for (var i = 0; i < results[0].address_components.length; i++) {
+           for (var b = 0; b < results[0].address_components[i].types.length; b++) {
+            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+              if (results[0].address_components[i].types[b] == 'locality') {
+                //this is the object you are looking for
+                city = results[0].address_components[i];
+                break;
+              }
+              if (results[0].address_components[i].types[b] == 'administrative_area_level_1') {
+                state = results[0].address_components[i];
+                break;
+              }
+            }
+          }
+          //city data
+          console.log('this is the city ', city.short_name);
+          console.log('this is the state ', state.short_name);
+          /*alert(city.short_name + ' ' + city.long_name)*/
+
+          userLocation.city = city;
+          userLocation.state = state;
+
+          /*$scope.city = city;
+          $scope.state = state;
+          $scope.short_name = $scope.city.short_name + ', ' + $scope.state.short_name;*/
+
+          callback(userLocation);
+
+          } else {
+            alert('No results found');
+          }
+        } else {
+          alert('Geocoder failed due to: ' + status);
+        }
+      });
+     }
+     initialize();
+   };
+
+   return {
+     locationInfo: locationInfo,
+     userLocation: userLocation
+   }
+
+})
